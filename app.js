4744cdd67f9920fe6dbd5bad9f2ad373ec4dbefb
Andrey Gnusarev <andreygnusarev@seacoastrealty.com>
2026-02-11 22:32:10 -0500

    answers for lesson 11

--- app.js
+++ app.js
@@ -40,10 +40,12 @@ app.use(xss());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
+}); */
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use(express.static("public"));
 
 // routes
 app.use('/api/v1/auth', authRouter);
